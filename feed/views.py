46352b50a6756d2790a4629a3b46277ca60e849e
Sreyajit Manna <sreyajit25@gmail.com>
2022-12-14 21:21:19 +0530

    commiting after a long time getting the hang of it

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
+
+class HomePage(ListView):
+    http_method_names = ["get"]
+    template_name = 'feed/homepage.html'
+    model= Post
+    context_object_name= "posts"
+    queryset = Post.objects.all().order_by('-id')[0:30]
+class PostDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name="feed/detail.html"
+    model = Post
+    context_object_name = "post"
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model= Post
+    template_name="feed/create.html"
+    fields = ["text"]
 
 # Create your views here.
